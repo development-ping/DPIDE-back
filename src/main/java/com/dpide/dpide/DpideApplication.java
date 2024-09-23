328974c65d1602b055486dcaa62f801c46f5f2ce
이민형 <iminhyeong@MacBook-Pro.local>
2024-09-23 16:26:54 +0900

    init project

---
+++ src/main/java/com/dpide/dpide/DpideApplication.java
@@ -0,0 +1,13 @@
+package com.dpide.dpide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpideApplication.class, args);
+	}
+
+}
